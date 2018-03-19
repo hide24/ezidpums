9a1f50cd135828e62b39ce53e26c8dbc0a6ce3df
hide24 <hidetoshi.yoshimoto@gmail.com>
2018-03-19 16:21:49 +0900

    add config/ititializers/00_constants.rb, bat not LDAP config because of ActiveLDAP bug. oops

---
+++ config/initializers/00_constants.rb
@@ -0,0 +1,17 @@
+#MySQL
+MYSQL_HOST = 'db'
+MYSQL_ROOT_USER = 'root'
+MYSQL_ROOT_PASSWORD = ENV['MYSQL_ROOT_PASSWORD']
+
+#LDAP
+LDAP_HOST = 'ldap'
+LDAP_BASE_DN = ENV['LDAP_BASE_DN']
+LDAP_BIND_DN = ENV['LDAP_BIND_DN']
+LDAP_BIND_PASSWORD = ENV['LDAP_BIND_PASSWORD']
+LDAP_ACCOUNT_BASE_DN_ALL = {
+  'production' => 'ou=production,' + LDAP_BASE_DN,
+  'development' => 'ou=development,' + LDAP_BASE_DN,
+  'test' => 'ou=test,' + LDAP_BASE_DN,
+}
+LDAP_ACCOUNT_BASE_DN = LDAP_ACCOUNT_BASE_DN_ALL[Rails.env]
+
