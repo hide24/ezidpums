2629d947aac2a7b68fe724dd1a1d9c8cebe37d97
hide24 <hidetoshi.yoshimoto@gmail.com>
2018-03-20 19:42:02 +0900

    docker base image was changed from ruby to phusion/passenger. now app works in production mode.

--- config/initializers/00_constants.rb
+++ config/initializers/00_constants.rb
@@ -1,13 +1,13 @@
 #MySQL
 MYSQL_HOST = 'db'
 MYSQL_ROOT_USER = 'root'
-MYSQL_ROOT_PASSWORD = ENV['MYSQL_ROOT_PASSWORD']
+MYSQL_ROOT_PASSWORD = ENV['MYSQL_ROOT_PASSWORD'] || 'password'
 
 #LDAP
 LDAP_HOST = 'ldap'
-LDAP_BASE_DN = ENV['LDAP_BASE_DN']
-LDAP_BIND_DN = ENV['LDAP_BIND_DN']
-LDAP_BIND_PASSWORD = ENV['LDAP_BIND_PASSWORD']
+LDAP_BASE_DN = ENV['LDAP_BASE_DN'] || 'dc=foo,dc=bar,dc=org'
+LDAP_BIND_DN = ENV['LDAP_BIND_DN'] || 'cn=admin,dc=foo,dc=bar,dc=org'
+LDAP_BIND_PASSWORD = ENV['LDAP_BIND_PASSWORD'] || 'password'
 LDAP_ACCOUNT_BASE_DN_ALL = {
   'production' => 'ou=production,' + LDAP_BASE_DN,
   'development' => 'ou=development,' + LDAP_BASE_DN,
@@ -16,9 +16,9 @@ LDAP_ACCOUNT_BASE_DN_ALL = {
 LDAP_ACCOUNT_BASE_DN = LDAP_ACCOUNT_BASE_DN_ALL[Rails.env]
 
 #IdP
-IDP_HOST_NAME = ENV['IDP_HOST_NAME']
+IDP_HOST_NAME = ENV['IDP_HOST_NAME'] || 'idp'
 IDP_ENTITY_ID = 'https://%s/idp/shibboleth' % IDP_HOST_NAME
-IDP_SCOPE = ENV['IDP_SCOPE']
+IDP_SCOPE = ENV['IDP_SCOPE'] || 'foo.org'
 SHIBBOLETH_CONFIG_DIR = '/opt/shibboleth-idp/conf'
 SHIBBOLETH_METADATA_DIR = '/opt/shibboleth-idp/metadata'
 SHIBBOLETH_CERT_DIR = '/opt/shibboleth-idp/credentials'
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+cd /home/app/ezidpums
+bundle exec rails db:create RAILS_ENV=production
+bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rails db:seed RAILS_ENV=production
+bundle exec rails runner 'ActiveLdap::Populate.ensure_base(Account, ou_base_class: ActiveLdap::Base)' RAILS_ENV=production
